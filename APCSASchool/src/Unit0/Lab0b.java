0c3ea416793eacfbb3e479f9dc2f5d9059ec3633
michael.remington <michael.remington@CA-SF-F106-01.sduhsd.lan>
2018-02-02 13:57:08 -0800

    Upload APCSASChool project

---
+++ APCSASchool/src/Unit0/Lab0b.java
@@ -0,0 +1,52 @@
+package Unit0;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0b
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		//1111 1111
+		//2^7 2^6....2^0
+		byte byteOne = 127;
+		short shortOne = 32123;
+		int intOne = 300000000;
+		long longOne = 300000000;
+		
+		//decimal variables
+		float floatOne = 30000000000000000000000.00000000000000f;
+		double doubleOne = 300000000000000000.00000000000;
+
+		//other integer types
+		char charOne = 'a';
+		char charTwo = 98;
+
+		//other types
+		boolean boolOne = true;
+		
+		String wordOne = "hello";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Jim Bob                08/18/06*");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+	}
+}
---
+++ APCSASchool/src/Unit0/Lab0c.java
@@ -0,0 +1,47 @@
+package Unit0;
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Lab0c
+{
+	public static void main (String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int intOne, intTwo;
+		double doubleOne, doubleTwo;
+		float floatOne, floatTwo;
+		short shortOne, shortTwo;
+
+
+		System.out.print("Enter an integer :: ");
+		intOne = keyboard.nextInt();
+
+
+		System.out.print("Enter an integer :: ");
+		intTwo = keyboard.nextInt();
+
+
+		//add in input for all variables
+
+
+
+
+
+
+		System.out.println();
+		System.out.println("integer one = " + intOne );
+		System.out.println("integer two = " + intTwo );
+
+		//add in output for all variables
+
+
+
+
+
+
+
+
+	}
+}
