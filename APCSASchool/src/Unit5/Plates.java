874afc92f839d413325661d4c5a93a1711311e37
mike <mike@DESKTOP-H98JHIB>
2018-02-13 21:27:04 -0800

    journal2-14

---
+++ APCSASchool/src/Unit5/Plates.java
@@ -0,0 +1,26 @@
+package Unit5;
+
+import java.util.Random;
+
+public class Plates {
+
+	public static void main(String[] args) {
+		String alpha = "abcdefghijklmnopqrstuvwxyz";
+		String nums ="0123456789";
+		
+		String plateNumber = "";
+		Random rand = new Random();
+		for(int i = 0; i < 3; i++){
+			plateNumber += alpha.charAt( rand.nextInt(25) );
+		}
+		
+		plateNumber = plateNumber.toUpperCase();
+		
+		for(int i = 0; i < 4; i++){
+			plateNumber += nums.charAt( rand.nextInt(10) );
+		}
+		
+		System.out.println(plateNumber);
+	}
+
+}
